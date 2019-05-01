df52d6a4382b8a797a549697d0991141b03d14a3
milad440550 <milad440550@gmail.com>
2019-05-01 21:47:29 +0200

    .

--- server.js
+++ server.js
@@ -20,8 +20,6 @@ db.once('open', () => {
 });
 
 app.use(express.static('./uploads'));
-if(process.env.NODE_ENV === 'production')
-    app.use(express.static("client/build"))
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
@@ -62,6 +60,14 @@ app.use((error, req, res, next) => {
     res.status(error.status || 500).json(error.message); 
     // exit();
 });
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static("client/build"))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
