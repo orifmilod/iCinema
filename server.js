fe2ba5eb3f2c84d9dd2bf1aac7393642af76c729
milad440550 <milad440550@gmail.com>
2019-05-02 12:26:26 +0200

    .

--- server.js
+++ server.js
@@ -64,13 +64,19 @@ app.use((error, req, res, next) => {
 
 //production mode
 if(process.env.NODE_ENV === 'production') {
-    console.log("PRODUCTIOn");
     app.use(express.static('client/build'));
     
-    app.get('*', (req, res) => {
-      res.sendfile(path.join(__dirname, 'client/build/index.html'));
+    app.get('/*', (req, res) => {
+      res.sendfile(path.join(__dirname, './client/build/index.html'));
     })
 }
+else {
+  app.use(express.static(path.join(__dirname, '/client/public')));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
+}
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
