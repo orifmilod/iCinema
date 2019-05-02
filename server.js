9169099c56a71fdf0b2294add89ba1b3b824117d
milad440550 <milad440550@gmail.com>
2019-05-02 11:26:14 +0200

    .

--- server.js
+++ server.js
@@ -61,9 +61,10 @@ app.use((error, req, res, next) => {
     res.status(error.status || 500).json(error.message); 
     // exit();
 });
-
-app.use('/', express.static(path.join(__dirname, '/client/build')));
-
+app.use(express.static(path.join(__dirname, "client", "build")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 // app.use('/', express.static(path.join(__dirname, '/client/build/index.html')));
 // if(process.env.NODE_ENV === 'production') {
     // app.use(express.static("/client/build"))
