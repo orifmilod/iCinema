895f3525c4f7a02e2c4d3be567d4d4823d3328c5
milad440550 <milad440550@gmail.com>
2019-10-20 11:07:25 +0200

    make it better

---
+++ config/keys_dev.js
@@ -0,0 +1,1 @@
+module.exports =  'mongodb+srv://admin:7fx6wiDzAGOOm3ZH@icinema-4p2wg.mongodb.net/iCinema?retryWrites=true'
--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = { 
-    mongoURI: process.env.MONGODB_URI,
-    secretOrKey: process.env.SECRET_OR_KEY
+  mongoURI: process.env.MONGODB_URI,
+  secretOrKey: process.env.SECRET_OR_KEY
 }
