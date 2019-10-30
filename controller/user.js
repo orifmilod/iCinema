94c1c7143795b35f80ad91f198caee05d3f13fb6
milad440550 <milad440550@gmail.com>
2019-10-30 20:38:13 +0100

    Added email sender from backend

--- controller/user.js
+++ controller/user.js
@@ -2,71 +2,89 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 
 
 exports.SIGN_UP = (req, res, next) => {
-    User
-    .find({ email: req.body.email })    //Checking if the email exist
-    .then(user => {
-        if(user.length > 0) res.status(409).json({ error: "The entered Email already exist!" })
+  const { email, password } = req.body;
+ 
+  User
+  .find({ email })    //Checking if the email exist
+  .then(user => {
+    if(user.length > 0) 
+      res.status(409).json({ error: "The entered Email already exist!" })
+    else {
+      //Hashing the password
+      bcrypt.hash(password, 10, (error, hash) => {
+        if(error) res.status(500).json({ error })
         else {
-            //Hashing the password
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
-                if(err) res.status(500).json({ error: err })
-                else {
-                    const newUser = new User({
-                        _id: mongoose.Types.ObjectId(),
-                        email: req.body.email,
-                        password: hash
-                    });
-
-                    newUser
-                    .save()
-                    .then(() => res.status(201).json({ message: "The user has been signed up successfully!" }))
-                    .catch((err) => res.status(500).json({ error: err }));
-                }
+          const userData = new User({_id: mongoose.Types.ObjectId(), email: email, password: hash, favouriteMovies: [] });
+          userData.save()
+          .then(() => { 
+            // create reusable transporter object using the default SMTP transport
+            let transporter = nodemailer.createTransport({
+              service: 'gmail',
+              port: 587,
+              secure: false, // true for 465, false for other ports
+              auth: {
+                user: process.env.SENDER_EMAIL, // generated ethereal user
+                pass: process.env.EMAIL_PASSWORD// generated ethereal password
+              }
             });
-        }
-    });
-}
+            transporter.sendMail({
+              from: process.env.SENDER_EMAIL,
+              to: `${email}`,
+              subject: 'Welcome to iCinema', 
+              text: `Hello Dear ${email}`, 
+              html: `<b>Hello Dear User, we are happy that you join our family. Kind Regards, iCinema Team.</b>` 
+            })
+            .then(info => console.log('Email has been sent!'))
+            .catch(err => console.log(err))
+            res.status(201).json({ message: "The user has been signed up successfully!", userData, favouriteMovies: [] })
+          }).catch(error => res.status(500).json({ error }));
+        } 
+    })
+    }
+  }
+)}
 
 exports.SIGN_IN = (req, res, next) => {
-    User
-    .find({ email: req.body.email }, (err, user) => {
-        if(err || user.length === 0) res.status(404).json({ error: "No user was found with this email." })
-        else if(user.length > 0) {
-        
-            //Comparing password
-            bcrypt.compare(req.body.password, user[0].password, (_err, result) => {
-                if(_err) res.status(401).json({ error: "Authentication has failed!" });
-                else if (result)
-                {
-                    const userData =  { email: user[0].email, ID: user[0]._id, favouriteMovies: user[0].favouriteMovies }
-                    const token = jwt.sign(userData, "MONGO_SECRET",{ expiresIn: '1h' } );
-                    res.status(200).json({ message: "Authentication has been successful", token: token, userData })
-                }
-                else res.status(401).json({ error: "The password entered is incorrect!" });
-            });
+  const { password, email } = req.body;
+  User
+  .find({ email: email }, (err, user) => {
+    if(err || user.length === 0) res.status(404).json({ error: "No user was found with this email." })
+    else if(user.length > 0) {      //Comparing password
+      bcrypt.compare(password, user[0].password, (_err, result) => {
+        if(_err) res.status(401).json({ error: "Authentication has failed!" });
+        else if (result) {
+          const userData = { email: user[0].email, ID: user[0]._id, favouriteMovies: user[0].favouriteMovies }
+          const token = jwt.sign(userData, "MONGO_SECRET", { expiresIn: '1h' } );
+          res.status(200).json({ message: "Authentication has been successful", token: token, userData })
         }
-    })
-    .catch((err) => res.status(500).json({ error: err }));
+        else res.status(401).json({ error: "The password entered is incorrect!" });
+      });
+    }
+  })
+  .catch((err) => res.status(500).json({ error: err }));
 }
 
 exports.UPDATE_USER = (req, res, next) => {
     const userID = req.params.userID;
 
     User
-    .updateMany({ _id: userID}, { $set: req.body })
+    .updateMany({ _id: userID }, { $set: req.body })
     .then(result => result.state(200).json(result)) 
     .catch(error => res.status(409).json(error))
 }
 
  
 exports.DELETE_USER = (req, res, next) => {
-    User.remove({ _id: req.params.userID })
-    .then(result => { 
-        if(result.length > 0) res.status(200).json({ message: "User has been deleted" }) 
-        else res.status(404).json({ message: "No user was found with this ID" })
-    })
-    .catch(error => res.status(200).json(error));
+  User.remove({ _id: req.params.userID })
+  .then(result => { 
+    if(result.length > 0) 
+      res.status(200).json({ message: "User has been deleted" }) 
+    else 
+      res.status(404).json({ message: "No user was found with this ID" })
+  })
+  .catch(error => res.status(200).json(error));
 }
