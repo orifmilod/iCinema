f539434474bdd9dc0a56b41277573e084467a74a
Orif Milod <orifmilod@gmail.com>
2020-07-15 22:38:10 +0200

    .

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,1 +1,2 @@
-module.exports =  'mongodb+srv://admin:7fx6wiDzAGOOm3ZH@icinema-4p2wg.mongodb.net/iCinema?retryWrites=true'
+module.exports =
+  "mongodb+srv://admin:7fx6wiDzAGOOm3ZH@icinema-4p2wg.mongodb.net/iCinema?retryWrites=true";
